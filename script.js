b5f5165043e1523108d7dae5d3612f38abaf7154
Renan Souza <renansouza0122@gmail.com>
2023-02-12 23:34:27 -0300

    function to do animation typing

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type",{ // class of the tag span
+    strings: ["Renan", "Studying" , "Coding"], // sentences to show 
+    typeSpeed:150, // speed of typing
+    backSpeed:150, // speed of backspace typing
+    loop:true // looping 
+})
